bf7dffa09b71b312b8fd6bbe8d24388c08b60583
masaya48 <msya0408@gmail.com>
2017-03-24 14:05:44 +0900

    モーダル追加

---
+++ fashion-map/src/app/detail/modal/modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maodal-component',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {
+}
--- fashion-map/src/app/resultMap/card/card.component.ts
+++ fashion-map/src/app/resultMap/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MdDialog } from '@angular/material';
+import { ModalComponent } from '../../detail/modal/modal.component';
 
 @Component({
   selector: 'card-component',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  constructor(public dialog: MdDialog) {}
+  
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
 }
